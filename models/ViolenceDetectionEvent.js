3eea81199e11af170db5ff549ae3d14baf9ce9ed
talnw1123 <talnw1123@gmail.com>
2025-01-24 00:56:02 +0700

    OKAY

--- models/ViolenceDetectionEvent.js
+++ models/ViolenceDetectionEvent.js
@@ -5,7 +5,11 @@ const Camera = require('./Camera');
 const ViolenceDetectionEvent = sequelize.define('ViolenceDetectionEvent', {
   event_time: { type: DataTypes.DATE, allowNull: false },
   description: { type: DataTypes.TEXT },
+<<<<<<< Updated upstream
   is_kept: { type: DataTypes.BOOLEAN, defaultValue: false },
+=======
+  is_resolved: { type: DataTypes.BOOLEAN, defaultValue: false },
+>>>>>>> Stashed changes
   created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 });
 
