a60fe4644b6409ad1dbacf96b5f87d11a03a30d3
talnw1123 <talnw1123@gmail.com>
2025-01-24 00:49:28 +0700

    Revert "OKAY"
    This reverts commit e04f7f6451532158edcaf717ece6fd1ff65a0e01.

--- routes/ForgottenItemRoute.js
+++ routes/ForgottenItemRoute.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< Updated upstream
 const returnItem = require('../controllers/ReturnItemController')
 const addForgottenItemEvent = require('../controllers/ReturnItemController')
 
 router.post('/add-forgotten-item', addForgottenItemEvent);
 router.put('/forgotten-item/:id/return', returnItem); //update ข้อมูล forgotten item
-=======
-const editForgottenItem = require('../controllers/EditForgottenController')
-const addForgottenItem = require('../controllers/AddForgottenItemController')
-
-router.post('/addForgottenItem', addForgottenItem);
-router.put('/editForgottenItem/:id', editForgottenItem); //update ข้อมูล forgotten item
->>>>>>> Stashed changes
 
 module.exports = router;
