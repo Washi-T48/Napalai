4382298391236dad008dbd7aa2db6d2a49775609
FoLkky60 <111549090+FoLkky60@users.noreply.github.com>
2025-01-31 15:03:27 +0700

    Chang videocard

--- Frontend/app/viewPage/cardLiveCamera.tsx
+++ Frontend/app/viewPage/cardLiveCamera.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import Image from "next/image";
-
 
 interface CardLiveCameraProps {
-  src: any;  
+  src: string;  
   camName: string;  
 }
 
 const CardLiveCamera: React.FC<CardLiveCameraProps> = ({ src, camName }) => {
   return (
     <div className="relative w-full h-full">
-      <Image src={src} alt={camName} className="w-full h-full object-cover" />
-      <div className="absolute bottom-0 left-0 bg-opacity-75 p-2 px-4 text-sm text-white  drop-shadow-2xl ">
+      <video src={src} autoPlay muted loop className="w-full h-full object-cover" />
+      <div className="absolute bottom-0 left-0 bg-opacity-75 p-2 px-4 text-sm text-white drop-shadow-2xl">
         {camName}
       </div>
     </div>
