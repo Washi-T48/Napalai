276b042464dcd4d77f8a561b84162ab02396573e
Pichayut Sombat <psb.nnt@gmail.com>
2025-02-14 00:16:55 +0700

    change utils duplicated keys
    camera.name as cameraName and zone.name as zoneName

--- Backend/models/utils.model.js
+++ Backend/models/utils.model.js
@@ -1,22 +1,22 @@
 import pool from "../config/db.js";
 
 const getForgottenList = async () => {
-    const rows = await pool.query('SELECT * FROM forgotten LEFT JOIN event ON forgotten.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id');
+    const rows = await pool.query('SELECT *, camera.name AS cameraName, zone.name AS zoneName FROM forgotten LEFT JOIN event ON forgotten.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id');
     return rows;
 };
 
 const getForgottenById = async (id) => {
-    const rows = await pool.query('SELECT * FROM forgotten LEFT JOIN event ON forgotten.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id WHERE forgotten.id = $1', [id]);
+    const rows = await pool.query('SELECT *, camera.name AS cameraName, zone.name AS zoneName FROM forgotten LEFT JOIN event ON forgotten.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id WHERE forgotten.id = $1', [id]);
     return rows;
 };
 
 const getViolenceList = async () => {
-    const rows = await pool.query('SELECT * FROM violence LEFT JOIN event ON violence.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id');
+    const rows = await pool.query('SELECT *, camera.name AS cameraName, zone.name AS zoneName FROM violence LEFT JOIN event ON violence.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id');
     return rows;
 };
 
 const getViolenceById = async (id) => {
-    const rows = await pool.query('SELECT * FROM violence LEFT JOIN event ON violence.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id WHERE violence.id = $1', [id]);
+    const rows = await pool.query('SELECT *, camera.name AS cameraName, zone.name AS zoneName FROM violence LEFT JOIN event ON violence.event_id = event.id LEFT JOIN camera ON event.camera_id = camera.id LEFT JOIN zone ON camera.zone_id = zone.id WHERE violence.id = $1', [id]);
     return rows;
 };
 
