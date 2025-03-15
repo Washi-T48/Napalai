8085d1a871c5d7ea9c19886e40269fb16c3630a4
Pichayut Sombat <psb.nnt@gmail.com>
2025-03-15 21:32:56 +0700

    Update upload.routes.js

--- Backend/routes/upload.routes.js
+++ Backend/routes/upload.routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import {
     forgottenImageUpload,
     forgottenVideoUpload,
@@ -11,46 +12,48 @@ import { updateForgottenImage, updateForgottenReturn, updateForgottenVideo } fro
 import { updateViolenceImage, updateViolenceVideo } from '../models/violence.model.js';
 import { updateProfilePicture } from '../models/user.model.js';
 
+const ROOT_URL = process.env.ROOT_URL;
+
 const uploadRouter = express.Router();
 
 uploadRouter.post('/forgotten/image', forgottenImageUpload.single('image'), async (req, res) => {
     const id = req.body.id;
-    const path = req.file.path;
+    const path = ROOT_URL + req.file.path;
     updateForgottenImage(id, path);
     res.status(200).send(path);
 });
 
 uploadRouter.post('/forgotten/video', forgottenVideoUpload.single('video'), async (req, res) => {
     const id = req.body.id;
-    const path = req.file;
+    const path = ROOT_URL + req.file.path;
     updateForgottenVideo(id, path);
     res.status(200).send(path);
 });
 
 uploadRouter.post('/forgotten/return', forgottenReturnUpload.single('return'), async (req, res) => {
     const id = req.body.id;
-    const path = req.file.path;
+    const path = ROOT_URL + req.file.path;
     updateForgottenReturn(id, path);
     res.status(200).send(path);
 });
 
 uploadRouter.post('/violence/image', violenceImageUpload.single('image'), async (req, res) => {
     const id = req.body.id;
-    const path = req.file.path;
+    const path = ROOT_URL + req.file.path;
     updateViolenceImage(id, path);
     res.status(200).send(path);
 });
 
 uploadRouter.post('/violence/video', violenceVideoUpload.single('video'), async (req, res) => {
     const id = req.body.id;
-    const path = req.file.path;
+    const path = ROOT_URL + req.file.path;
     updateViolenceVideo(id, path);
     res.status(200).send(path);
 });
 
 uploadRouter.post('/profile', profilePictureUpload.single('image'), async (req, res) => {
     const id = req.body.id;
-    const path = req.file.path;
+    const path = ROOT_URL + req.file.path;
     updateProfilePicture(id, path);
     res.status(200).send(path);
 });
