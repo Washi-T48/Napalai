65f172b7bf794b853219e3a8f9b721c25c308a5c
Pichayut Sombat <psb.nnt@gmail.com>
2025-03-18 22:47:30 +0700

    HOTFIX UPLOAD NO TRY CATCH
    THE SERVER DIED ON ME WHILE I WAS BEING ASLEEP. THANK YOU MR.ALARM CLOCK

--- Backend/routes/upload.routes.js
+++ Backend/routes/upload.routes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import {
     forgottenImageUpload,
     forgottenVideoUpload,
@@ -17,45 +16,69 @@ const ROOT_URL = process.env.ROOT_URL;
 const uploadRouter = express.Router();
 
 uploadRouter.post('/forgotten/image', forgottenImageUpload.single('image'), async (req, res) => {
-    const id = req.body.id;
-    const path = ROOT_URL + req.file.path;
-    updateForgottenImage(id, path);
-    res.status(200).send(path);
+    try {
+        const id = req.body.id;
+        const path = ROOT_URL + req.file.path;
+        updateForgottenImage(id, path);
+        res.status(200).send(path);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 uploadRouter.post('/forgotten/video', forgottenVideoUpload.single('video'), async (req, res) => {
-    const id = req.body.id;
-    const path = ROOT_URL + req.file.path;
-    updateForgottenVideo(id, path);
-    res.status(200).send(path);
+    try {
+        const id = req.body.id;
+        const path = ROOT_URL + req.file.path;
+        updateForgottenVideo(id, path);
+        res.status(200).send(path);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 uploadRouter.post('/forgotten/return', forgottenReturnUpload.single('return'), async (req, res) => {
-    const id = req.body.id;
-    const path = ROOT_URL + req.file.path;
-    updateForgottenReturn(id, path);
-    res.status(200).send(path);
+    try {
+        const id = req.body.id;
+        const path = ROOT_URL + req.file.path;
+        updateForgottenReturn(id, path);
+        res.status(200).send(path);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 uploadRouter.post('/violence/image', violenceImageUpload.single('image'), async (req, res) => {
-    const id = req.body.id;
-    const path = ROOT_URL + req.file.path;
-    updateViolenceImage(id, path);
-    res.status(200).send(path);
+    try {
+        const id = req.body.id;
+        const path = ROOT_URL + req.file.path;
+        updateViolenceImage(id, path);
+        res.status(200).send(path);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 uploadRouter.post('/violence/video', violenceVideoUpload.single('video'), async (req, res) => {
-    const id = req.body.id;
-    const path = ROOT_URL + req.file.path;
-    updateViolenceVideo(id, path);
-    res.status(200).send(path);
+    try {
+        const id = req.body.id;
+        const path = ROOT_URL + req.file.path;
+        updateViolenceVideo(id, path);
+        res.status(200).send(path);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 uploadRouter.post('/profile', profilePictureUpload.single('image'), async (req, res) => {
-    const id = req.body.id;
-    const path = ROOT_URL + req.file.path;
-    updateProfilePicture(id, path);
-    res.status(200).send(path);
+    try {
+        const id = req.body.id;
+        const path = ROOT_URL + req.file.path;
+        updateProfilePicture(id, path);
+        res.status(200).send(path);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 export default uploadRouter;
