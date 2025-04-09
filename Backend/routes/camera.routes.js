b254eb647a834b44f5f4db4a7cfb0e0575bbcac5
Pichayut Sombat <psb.nnt@gmail.com>
2025-04-09 23:12:42 +0700

    Update camera.routes.js

--- Backend/routes/camera.routes.js
+++ Backend/routes/camera.routes.js
@@ -16,7 +16,7 @@ const cameraRouter = express.Router();
 cameraRouter.get('/', async (req, res) => {
     try {
         const cameras = await getAllCameras();
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(200).json(cameras.rows);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -28,7 +28,7 @@ cameraRouter.post('/', async (req, res) => {
         const camera = req.body;
         const newCamera = await createCamera(camera);
         await addAllCameraFromDatabase();
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(201).json(newCamera.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -39,7 +39,7 @@ cameraRouter.get('/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const camera = await getCameraById(id);
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(200).json(camera.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -52,7 +52,7 @@ cameraRouter.put('/:id', async (req, res) => {
         const camera = req.body;
         const updatedCamera = await updateCamera(id, camera);
         await addAllCameraFromDatabase();
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(200).json(updatedCamera.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -64,7 +64,7 @@ cameraRouter.patch('/:id/rename', async (req, res) => {
         const id = req.params.id;
         const camera = req.body;
         const updatedCamera = await renameCamera(id, camera);
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(200).json(updatedCamera.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -76,7 +76,7 @@ cameraRouter.patch('/:id/zone', async (req, res) => {
         const id = req.params.id;
         const camera = req.body;
         const updatedCamera = await changeCameraZone(id, camera);
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(200).json(updatedCamera.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -87,7 +87,7 @@ cameraRouter.delete('/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const deletedCamera = await deleteCamera(id);
-        updateAllCameraDBPath(cameras.rows);
+        updateAllCameraDBPath();
         res.status(200).json(deletedCamera.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
